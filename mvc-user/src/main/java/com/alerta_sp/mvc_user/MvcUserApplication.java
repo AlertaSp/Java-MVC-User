898bea3b40e2f8300a3d28cee7874c8006113a6c
Sabrina da Motta Café <sabis476@gmail.com>
2025-05-26 20:59:59 -0300

    🚀 Projeto inicial - Spring MVC com Thymeleaf

---
+++ mvc-user/src/main/java/com/alerta_sp/mvc_user/MvcUserApplication.java
@@ -0,0 +1,13 @@
+package com.alerta_sp.mvc_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcUserApplication.class, args);
+	}
+
+}
---
+++ mvc-user/src/test/java/com/alerta_sp/mvc_user/MvcUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alerta_sp.mvc_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
